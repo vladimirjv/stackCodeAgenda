a66600a284bba63ecee1c53e45c3178d95d9e877
vladimirjv <ivladimirjuarez@outlook.com>
2019-02-12 08:27:20 -0600

    actualizando para validación

--- src/pages/singleContact.vue
+++ src/pages/singleContact.vue
@@ -16,6 +16,8 @@
               <q-input
                 v-model="name"
                 float-label="Nombre"
+                @blur="$v.name.$touch"
+                :error= "$v.name.$error"
                 clearable
               />
             </q-field>
@@ -26,6 +28,8 @@
                 v-model="phone"
                 type="number"
                 float-label="Telefono"
+                @blur="$v.phone.$touch"
+                :error="$v.phone.$error"
                 clearable
               />
             </q-field>
@@ -36,6 +40,8 @@
                 v-model="email"
                 type="email"
                 float-label="Email"
+                @blur="$v.email.$touch"
+                :error="$v.email.$error"
                 clearable
                 suffix="@email.com"
               />
@@ -66,6 +72,7 @@
 
 <script>
 import * as Notifications from '../assets/notifications.js';
+import { required, email, numeric, maxLength } from 'vuelidate/lib/validators';
 export default {
   // name: 'PageName',
   data() {
@@ -76,8 +83,23 @@ export default {
       createdDate: '',
     }
   },
+  validations: {
+    name: {
+      required,
+      maxLength: maxLength(18)
+    },
+    phone: {
+      required,
+      maxLength: maxLength(10)
+    },
+    email: {
+      required,
+      maxLength: maxLength(80)
+    }
+  },
   methods: {
     getInfo(){
+
       var self = this;
       this.$axios.get('/contacts/'+this.$route.params.id)
         .then(function (response) {
@@ -88,9 +110,24 @@ export default {
         .catch(function (err) {
           console.log(err);
         })
+
     },
     update(){
+
+      this.touch();
       var self = this;
+      if (this.$v.name.$error || this.$v.phone.$error || this.$v.email.$error) {
+        this.showNotification(Notifications.errorNotification);
+        return
+      }
+      // if (this.$v.phone.$error) {
+      //   this.showNotification(Notifications.errorNotification);
+      //   return
+      // }
+      // if (this.$v.email.$error) {
+      //   this.showNotification(Notifications.errorNotification);
+      //   return
+      // }
       this.$axios.put('/contacts/'+this.$route.params.id,{
         name: this.name,
         phone: this.phone,
@@ -102,14 +139,21 @@ export default {
       .catch(function (err) {
         console.log(err);
       });
+
     },
     deleteMethod(){
+
       var self= this;
       this.$axios.delete('/contacts/'+this.$route.params.id)
         .then(function (response) {
           self.showNotification(Notifications.deleteNotification);
           self.$router.push('/')
         })
+    },
+    touch(){
+      this.$v.name.$touch();
+      this.$v.phone.$touch();
+      this.$v.email.$touch();
     },
     showNotification(notification){
       this.$q.notify(notification)
@@ -118,6 +162,7 @@ export default {
   created() {
     this.getInfo();
   },
+
 }
 </script>
 
