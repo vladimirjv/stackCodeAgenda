87c807250e3082fc06fd7bd649c790dc159ba290
vladimirjv <ivladimirjuarez@outlook.com>
2019-02-12 07:29:33 -0600

    agregando vuelidate

--- quasar.conf.js
+++ quasar.conf.js
@@ -3,7 +3,8 @@ module.exports = function (ctx) {
   return {
     // app plugins (/src/plugins)
     plugins: [
-      'axios'
+      'axios',
+      'vuelidate'
     ],
     css: [
       'app.styl'
---
+++ src/plugins/vuelidate.js
@@ -0,0 +1,6 @@
+/* jshint esversion: 6 */
+import Vuelidate from 'vuelidate';
+
+export default ({Vue}) => {
+  Vue.use(Vuelidate);
+};
