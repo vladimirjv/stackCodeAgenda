fe394724f8076a95fa048f358f45550d915b902c
vladimirjv <ivladimirjuarez@outlook.com>
2019-02-12 00:29:49 -0600

    axios url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export default ({ Vue }) => {
-  // axios.defaults.baseURL='https://stackcode-vla.herokuapp.com/api';
-  axios.defaults.baseURL = 'http://localhost:3000/api';
+  axios.defaults.baseURL='https://stackcode-vla.herokuapp.com/api';
+  // axios.defaults.baseURL = 'http://localhost:3000/api';
   Vue.prototype.$axios = axios;
 };
